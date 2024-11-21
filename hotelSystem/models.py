dcfa72a2b0b960ceffb4162e7173479d6a4c0a43
Kasprzyk.Szymon <kasprzyk.szymon.99@gmail.com>
2024-11-21 21:43:12 +0100

    start with models

--- hotelSystem/models.py
+++ hotelSystem/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# class room(models.Model):
+#     nummber_of_people = people
+#     date_of_reservation = date_of_reservation
+
+# class guest(models.Model):
+#         self.name = name
+#         self.surname = surname
+#         self.country = country
+#         self.phone_number = phone_number
+# class client(quest):
+#     def (self, login, password, client_e_mail):
+#         self.login = login
+#         self.password = password
+#         self.client_e_mail = client_e_mail
+
+
+
+
