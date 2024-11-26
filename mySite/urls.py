5878b791147a7e263eb219bf0ac614ac9fb8cab5
Kasprzyk.Szymon <kasprzyk.szymon.99@gmail.com>
2024-11-26 23:47:09 +0100

    Add news views in navigate homepage

--- mySite/urls.py
+++ mySite/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hotelSystem.urls'))
-
+    path('', include('hotelSystem.urls')),
+    path('lastminute/', include('hotelSystem.urls')),
+    path('news/', include('hotelSystem.urls')),
+    path('contact/', include('hotelSystem.urls')),
+    path('signin/', include('hotelSystem.urls')),
+    path('searchroom/', include('hotelSystem.urls')),
 ]
