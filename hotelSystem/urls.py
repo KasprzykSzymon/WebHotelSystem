d58cfee673592279641836efbcc430e34566b2de
Kasprzyk.Szymon <kasprzyk.szymon.99@gmail.com>
2024-11-26 23:47:09 +0100

    Add news views in navigate homepage

--- hotelSystem/urls.py
+++ hotelSystem/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homePage, name='homePage')
+    path('', views.homePage, name='homePage'),
+    path('lastminute/', views.lastMinute, name='lastMinute'),
+    path('news/', views.news, name='news'),
+    path('contact/', views.contact, name='contact'),
+    path('signin/', views.signIn, name='signIn'),
+    path('searchroom/', views.searchRoom, name='searchRoom'),
+
 ]
--- hotelSystem/views.py
+++ hotelSystem/views.py
@@ -5,5 +5,36 @@ def homePage(request):
         'range_10': range(0, 11),
         'range_10x': range(1, 11),
     }
-    
     return  render(request, 'homePage.html', context)
+
+def lastMinute(request):
+    context = {
+        'range_10': range(0, 11),
+        'range_10x': range(1, 11),
+    }
+    return render(request, 'lastMinute.html', context)
+
+def news(request):
+    context = {
+        'range_10': range(0, 11),
+        'range_10x': range(1, 11),
+    }
+    return render(request, 'news.html', context)
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def signIn(request):
+    context = {
+        'range_10': range(0, 11),
+        'range_10x': range(1, 11),
+    }
+    return render(request, 'signIn.html', context)
+
+def searchRoom(request):
+    context = {
+        'range_10': range(0, 11),
+        'range_10x': range(1, 11),
+    }
+    return render(request, 'searchRoom.html')
