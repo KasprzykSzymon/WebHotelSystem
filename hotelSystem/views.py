eebe46ec59a29e9f4f41053595cd8b4c6f015a61
Mateusz W <mateusz.wisniewskips@gmail.com>
2024-11-26 17:59:03 +0100

    modified:   hotelSystem/Templates/homePage.html 	modified:   hotelSystem/static/css/stylesHomePage.css 	new file:   hotelSystem/static/images/Gory.jpg     new file:   hotelSystem/static/images/Hotel.jpg 	new file:   hotelSystem/static/images/Kielce.jpg 	new file:   hotelSystem/static/images/LogoJPG.jpg 	new file:   hotelSystem/static/images/LogoPNG.png 	new file:   hotelSystem/static/images/zdjHotelu.png 	modified:   hotelSystem/views.py

--- hotelSystem/views.py
+++ hotelSystem/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def homePage(request):
-    return  render(request, 'homePage.html', {})
+    context = {
+        'range_10': range(0, 11),
+        'range_10x': range(1, 11),
+    }
+    
+    return  render(request, 'homePage.html', context)
