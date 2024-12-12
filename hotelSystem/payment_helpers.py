c47eb13523280c4ee1982515ccac8e788854a832
Oskar P <pytlewski.oskar@gmail.com>
2024-12-12 09:04:20 +0100

    Updated models.py and added the last_minute logic

---
+++ hotelSystem/payment_helpers.py
@@ -0,0 +1,52 @@
+import base64
+import hashlib
+import hmac
+import json
+
+import requests
+data_to_hash = "53c59ed5-64b8-44ee-8db7-3d0e555c5eea"
+key = "70d0fac2-c3bf-43c1-b55f-208698e43eb7"
+
+def calculate_hmac(data, key):
+    hashed_object = hmac.new(key, data, hashlib.sha256).digest()
+    return base64.b64encode(hashed_object)
+
+
+hmac_hash = calculate_hmac(data_to_hash.encode(), key.encode())
+# Example structure from API doc
+
+def new_payment(data, idempotency_key):
+    data = json.dumps(data)
+    s = requests.Session()
+    print(key)
+    print(data)
+    print(data_to_hash)
+    req = requests.Request('POST', "https://api.sandbox.paynow.pl/v1/payments",
+                           {'Api-key': key, "Signature": calculate_hmac(data.encode(), data_to_hash.encode()),
+                            'Idempotency-Key': idempotency_key, 'Accept': '*/*', 'Content-Type': 'application/json'},
+                           data=data).prepare()
+    r = s.send(req)
+    return (json.loads(r.text))
+
+
+def check_payment(payment_id):
+    s = requests.Session()
+    req = requests.Request('GET', "https://api.sandbox.paynow.pl/v1/payments/{}/status".format(payment_id),
+                           {'Api-key': key, 'Accept': '*/*', 'Content-Type': 'application/json'}).prepare()
+    r = s.send(req)
+    return (json.loads(r.text))
+
+# print(new_payment({
+#         "amount": 45671,
+#         "description": "Some description",
+#         "externalId": "234567898654",
+#         "buyer": {
+#             "email": "aaa@bbb.pl",
+#             "phone": {
+#                 "prefix": "+48",
+#                 "number": 123456789
+#             }
+#         }
+#     }, "test-mateusz-1001"))
+
+# print(check_payment("NOEE-F4V-2BB-S5C"))
